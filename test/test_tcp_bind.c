1012302b67bc4f0f408db1a5775c71927adf694e
qgymib <qgymib@gmail.com>
2020-12-25 21:11:05 +0800

    test: bind local address

--- test/test_tcp_bind.c
+++ test/test_tcp_bind.c
@@ -23,7 +23,7 @@ TEST(tcp_bind)
 	struct sockaddr_in addr;
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addr.sin_port = htons(0);
 	ASSERT_EQ_D32(ev_tcp_bind(&s_sock, (struct sockaddr*)&addr, sizeof(addr)), 0);
 
