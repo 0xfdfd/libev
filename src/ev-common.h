495499c64bc8b2304777e0d77d1d26a1c896f711
qgymib <qgymib@gmail.com>
2020-12-27 13:27:14 +0800

    add extra check for #container_of

--- src/ev-common.h
+++ src/ev-common.h
@@ -7,8 +7,18 @@ extern "C" {
 #include <stdlib.h>
 #include "ev.h"
 
-#define container_of(ptr, type, member) \
-  ((type *) ((char *) (ptr) - offsetof(type, member)))
+#if !defined(container_of)
+#	if defined(__GNUC__) || defined(__clang__)
+#		define container_of(ptr, type, member) \
+			({ \
+				const typeof(((type *)0)->member)*__mptr = (ptr); \
+				(type *)((char *)__mptr - offsetof(type, member)); \
+			})
+#	else
+#		define container_of(ptr, type, member) \
+			((type *) ((char *) (ptr) - offsetof(type, member)))
+#	endif
+#endif
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
