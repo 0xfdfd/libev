8543b1f5b52ea289e51ea9e9692f99126ff25c8d
qgymib <qgymib@gmail.com>
2021-11-21 17:59:50 +0800

    test: tcp.empty_loop / tcp.connect_non_exist

---
+++ test/src/test.c
@@ -0,0 +1,90 @@
+#define _GNU_SOURCE
+#include "test.h"
+#include <errno.h>
+#include <stdio.h>
+
+#if defined(_MSC_VER)
+
+static DWORD WINAPI _thread_proc(LPVOID lpParameter)
+{
+	fn_execute callback = lpParameter;
+	callback();
+	return 0;
+}
+
+int test_thread_execute(test_execute_token_t* token, fn_execute callback)
+{
+	token->thr_handle = CreateThread(NULL, 0, _thread_proc, callback, 0, NULL);
+	return token->thr_handle != INVALID_HANDLE_VALUE ? 0 : -1;
+}
+
+int test_thread_wait(test_execute_token_t* token)
+{
+	DWORD ret = WaitForSingleObject(token->thr_handle, 0);
+	switch (ret)
+	{
+	case WAIT_ABANDONED:
+		abort();
+		break;
+
+		/* thread finish */
+	case WAIT_OBJECT_0:
+		return 0;
+
+		/* not finish */
+	case WAIT_TIMEOUT:
+		return 1;
+
+		/* error */
+	case WAIT_FAILED:
+	default:
+		break;
+	}
+	return -1;
+}
+
+void test_thread_sleep(unsigned ms)
+{
+	Sleep(ms);
+}
+
+#else
+#	include <unistd.h>
+
+static void* _task_body(void* arg)
+{
+	fn_execute callback = arg;
+	callback();
+	return NULL;
+}
+
+int test_thread_execute(test_execute_token_t* token, fn_execute callback)
+{
+	return pthread_create(&token->thr_handle, NULL, _task_body, callback);
+}
+
+int test_thread_wait(test_execute_token_t* token)
+{
+	int ret = pthread_tryjoin_np(token->thr_handle, NULL);
+
+	//printf("ret:%d\n", ret);
+	switch (ret)
+	{
+		/* terminated */
+	case 0:
+		return 0;
+		/* not finish */
+	case EBUSY:
+		return 1;
+	default:
+		return -1;
+	}
+}
+
+void test_thread_sleep(unsigned ms)
+{
+	usleep(ms * 1000);
+}
+
+#endif
+
--- test/src/test.h
+++ test/src/test.h
@@ -5,6 +5,53 @@ extern "C" {
 #endif
 
 #include "cutest.h"
+#if defined(_MSC_VER)
+#	include <windows.h>
+#else
+#	include <pthread.h>
+#endif
+
+#define TEST_T(fixture_name, case_name, timeout)	\
+	static void TEST_##fixture_name##_##case_name##_timeout(void);\
+	TEST_F(fixture_name, case_name) {\
+		const uint64_t timeout_ms = (timeout);\
+		test_execute_token_t token;\
+		ASSERT_EQ_D32(test_thread_execute(&token,\
+			TEST_##fixture_name##_##case_name##_timeout), 0);\
+		cutest_timestamp_t t_start;\
+		cutest_timestamp_get(&t_start);\
+		int ret;\
+		while ((ret = test_thread_wait(&token)) > 0) {\
+			cutest_timestamp_t t_current, t_diff;\
+			cutest_timestamp_get(&t_current);\
+			cutest_timestamp_dif(&t_current, &t_start, &t_diff);\
+			uint64_t spend_time_ms = t_diff.sec * 1000 + t_diff.usec / 1000;\
+			ASSERT_LE_U64(spend_time_ms, timeout_ms);\
+			test_thread_sleep(1);\
+		}\
+		ASSERT_EQ_D32(ret, 0, "task not finished");\
+	}\
+	static void TEST_##fixture_name##_##case_name##_timeout(void)
+
+typedef void (*fn_execute)(void);
+
+typedef struct test_execute_token
+{
+#if defined(_MSC_VER)
+	HANDLE		thr_handle;
+#else
+	pthread_t	thr_handle;
+#endif
+}test_execute_token_t;
+
+int test_thread_execute(test_execute_token_t* token, fn_execute callback);
+
+/**
+ * @return 0: thread finish, -1:error, 1: not finish
+ */
+int test_thread_wait(test_execute_token_t* token);
+
+void test_thread_sleep(unsigned ms);
 
 #ifdef __cplusplus
 }
