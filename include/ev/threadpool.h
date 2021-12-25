c7f3e1ff804c3eda6088faea3b389c35924300d2
qgymib <qgymib@gmail.com>
2021-12-25 14:38:45 +0800

    add documents

--- include/ev/threadpool.h
+++ include/ev/threadpool.h
@@ -12,14 +12,28 @@
 extern "C" {
 #endif
 
+/**
+ * @defgroup EV_THREAD_POOL Thread pool
+ * @{
+ */
+
 struct ev_threadpool;
 typedef struct ev_threadpool ev_threadpool_t;
 
 struct ev_threadpool_work;
 typedef struct ev_threadpool_work ev_threadpool_work_t;
 
-typedef void (*ev_threadpool_close_cb)(ev_threadpool_t* pool);
+/**
+ * @brief Thread pool task
+ * @param[in] work  Work token
+ */
 typedef void (*ev_threadpool_work_cb)(ev_threadpool_work_t* work);
+
+/**
+ * @brief Work done callback in event loop
+ * @param[in] work      Work token
+ * @param[in] status    Work status
+ */
 typedef void (*ev_threadpool_work_done_cb)(ev_threadpool_work_t* work, int status);
 
 typedef enum ev_threadpool_work_type
@@ -42,31 +56,31 @@ typedef enum ev_threadpool_work_type
 
 struct ev_threadpool
 {
-    ev_mutex_t                      mutex;
-    ev_sem_t                        ent_sem;
-    ev_sem_t                        p2w_sem;
-    ev_sem_t                        w2p_sem;
+    ev_mutex_t                      mutex;          /**< Thread pool mutex */
+    ev_sem_t                        ent_sem;        /**< Semaphore for thread entrypoint */
+    ev_sem_t                        p2w_sem;        /**< Semaphore for pool to worker */
+    ev_sem_t                        w2p_sem;        /**< Semaphore for worker to pool */
 
     ev_os_thread_t*                 threads;        /**< Threads */
     size_t                          thrnum;         /**< The number of threads */
 
-    int                             looping;
+    int                             looping;        /**< Looping flag */
 
-    ev_cycle_list_node_t            cpu_queue;
-    ev_cycle_list_node_t            io_fast_queue;
-    ev_cycle_list_node_t            io_slow_queue;
+    ev_cycle_list_node_t            cpu_queue;      /**< work queue for #EV_THREADPOOL_WORK_CPU */
+    ev_cycle_list_node_t            io_fast_queue;  /**< work queue for #EV_THREADPOOL_WORK_IO_FAST */
+    ev_cycle_list_node_t            io_slow_queue;  /**< work queue for #EV_THREADPOOL_WORK_IO_SLOW */
 };
 
 struct ev_threadpool_work
 {
     ev_handle_t                     base;           /**< Base object */
 
-    ev_cycle_list_node_t            node;
-    ev_todo_t                       token;
+    ev_cycle_list_node_t            node;           /**< List node */
+    ev_todo_t                       token;          /**< Callback token */
     struct
     {
-        ev_threadpool_t*            pool;
-        ev_loop_t*                  loop;
+        ev_threadpool_t*            pool;           /**< Thread pool */
+        ev_loop_t*                  loop;           /**< Event loop */
 
         /**
          * @brief Work status.
@@ -76,8 +90,9 @@ struct ev_threadpool_work
          * + #EV_SUCCESS:   Done
          */
         int                         status;
-        ev_threadpool_work_cb       work_cb;
-        ev_threadpool_work_done_cb  done_cb;
+
+        ev_threadpool_work_cb       work_cb;        /**< work callback */
+        ev_threadpool_work_done_cb  done_cb;        /**< done callback */
     }data;
 };
 
@@ -120,6 +135,10 @@ int ev_threadpool_submit(ev_threadpool_t* pool, ev_loop_t* loop,
  * @return              #ev_errno_t
  */
 int ev_threadpool_cancel(ev_threadpool_work_t* token);
+
+/**
+ * @} EV_THREAD_POOL
+ */
 
 #ifdef __cplusplus
 }
