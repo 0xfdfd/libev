e77c37c79facfa2d070aeb39b4dfbccf9f9288aa
qgymib <qgymib@gmail.com>
2022-05-10 11:54:55 +0000

    add note for #ev__fs_preadv() / #ev__fs_pwritev()

--- src/fs-common.h
+++ src/fs-common.h
@@ -29,27 +29,32 @@ API_LOCAL int ev__fs_close(ev_os_file_t file);
  * @param[in] mode      Creation mode.
  * @return              #ev_errno_t
  */
-API_LOCAL int ev__fs_open(ev_os_file_t* file, const char* path, int flags, int mode);
+API_LOCAL int ev__fs_open(ev_os_file_t* file, const char* path, int flags,
+    int mode);
 
 /**
  * @brief Same as [preadv(2)](https://linux.die.net/man/2/preadv)
+ * @note For windows users, the \p file can NOT open with FILE_FLAG_OVERLAPPED.
  * @param[in] file      File handle.
  * @param[in] bufs      Buffer list
  * @param[in] nbuf      Buffer number
  * @param[in] offset    Offset.
  * @return              #ev_errno_t
  */
-API_LOCAL ssize_t ev__fs_preadv(ev_os_file_t file, ev_buf_t* bufs, size_t nbuf, ssize_t offset);
+API_LOCAL ssize_t ev__fs_preadv(ev_os_file_t file, ev_buf_t* bufs, size_t nbuf,
+    ssize_t offset);
 
 /**
  * @brief Same as [pwritev(2)](https://linux.die.net/man/2/pwritev)
+ * @note For windows users, the \p file can NOT open with FILE_FLAG_OVERLAPPED.
  * @param[in] file      File handle.
  * @param[in] bufs      Buffer list
  * @param[in] nbuf      Buffer number
  * @param[in] offset    Offset.
  * @return              #ev_errno_t
  */
-API_LOCAL ssize_t ev__fs_pwritev(ev_os_file_t file, ev_buf_t* bufs, size_t nbuf, ssize_t offset);
+API_LOCAL ssize_t ev__fs_pwritev(ev_os_file_t file, ev_buf_t* bufs, size_t nbuf,
+    ssize_t offset);
 
 /**
  * @brief Same as [fstat(2)](https://linux.die.net/man/2/fstat)
