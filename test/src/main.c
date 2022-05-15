f5d6efae3fb1a9a9cfb993144b28ee0619bb8d8f
qgymib <qgymib@gmail.com>
2022-05-15 22:36:21 +0800

    ensure all output was done

--- test/src/main.c
+++ test/src/main.c
@@ -1,12 +1,20 @@
 #include "test.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+
+static void _at_exit(void)
+{
+    /* ensure all output was done. */
+    fflush(NULL);
+}
 
 int main(int argc, char* argv[])
 {
 #if defined(SIGPIPE)
     signal(SIGPIPE, SIG_IGN);
 #endif
+    atexit(_at_exit);
     return cutest_run_tests(argc, argv, &test_hook);
 }
